2b66b0fa219f3905b30ff0f35b6d13dd715248a7
Jonathan Robinson <jdswebservice@gmail.com>
2018-09-26 18:18:18 -0400

    Lession 2: Creating our Create View & Storing Data In Our Database

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TasksController extends Controller
 {
@@ -23,7 +25,7 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        return view('tasks.create');
     }
 
     /**
@@ -34,7 +36,29 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate The Data
+        $this->validate($request, [
+            'name' => 'required|string|max:255|min:3',
+            'description' => 'required|string|max:10000|min:10',
+            'due_date' => 'required|date',
+        ]);
+
+        // Create a New task
+        $task = new Task;
+
+        // Assign the Task data from our request
+        $task->name = $request->name;
+        $task->description = $request->description;
+        $task->due_date = $request->due_date;
+
+        // Save the task
+        $task->save();
+
+        // Flash Session Message with Success
+        Session::flash('success', 'Created Task Successfully');
+
+        // Return A Redirect
+        return redirect()->route('task.create');
     }
 
     /**
